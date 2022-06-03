bde8d6e7e273267d2f5578c3c3e7bcdc38d6cee0
jmhs11 <josemariahs@gmail.com>
2022-06-03 15:35:22 +0200

    Add Ladle and Tailwind

---
+++ .ladle/components.jsx
@@ -0,0 +1,3 @@
+import '../src/index.css';
+
+export const Provider = ({ children, globalState }) => <>{children}</>;
---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,5 @@
+export const Button = () => (
+	<button className='bg-red-500 rounded text-white px-4 py-2 dark:bg-blue-500'>
+		My Button
+	</button>
+);
